09b1d2c5464b6556c61abeed4ea0dee0d3771707
nhun0005 <142361107+nhun0005@users.noreply.github.com>
2023-09-19 19:38:31 +1000

    Changes made via VSC

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "avg_precip_bubble_map.vg.json";
+vegaEmbed("#bubble_map", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
